60db28e69ec801d85b7e07c9eb0d0fc8f046d9f4
Mark Carlebach <markcarlebach@macbook-pro.myfiosgateway.com>
2022-05-12 09:33:02 -0400

    first attempt at adding DT model and new worker--not yet tested

--- run_workers.py
+++ run_workers.py
@@ -1,6 +1,28 @@
 import asyncio
 from pyzeebe import ZeebeWorker, create_camunda_cloud_channel
+import pickle
 
+####################################################
+#      SETUP TO USE DT MODEL
+####################################################
+model_dir = "../ML_resolver/models/"
+
+file_name = "feature_enc_model.serial"
+with open(f"{model_dir}{file_name}", "rb") as f:
+    feature_enc = pickle.load(f)
+
+file_name = "label_enc_model.serial"
+with open(f"{model_dir}{file_name}", "rb") as f:
+    label_enc = pickle.load(f)
+
+file_name = "dt_model.serial"
+with open(f"{model_dir}{file_name}", "rb") as f:
+    dt_model = pickle.load(f)
+
+
+####################################################
+#      SETUP TO USE C8 ZEEBE
+####################################################
 # Create channel to Zeebe
 channel = create_camunda_cloud_channel(
     client_id="Dk0MPLoP_F0CmECfoidErdBdcLzZxLr.",
@@ -10,6 +32,9 @@ channel = create_camunda_cloud_channel(
 # Create single threaded worker
 worker = ZeebeWorker(channel)
 
+####################################################
+#          DEFINE WORKERS
+####################################################
 # Define work this client should do when trade_match_worker job exists in Zeebe
 @worker.task(task_type="trade_match_worker")
 async def trade_match_work(trans_ref,
@@ -34,12 +59,50 @@ async def trade_match_work(trans_ref,
                            _merge
                            ):
 
-    print(f"merge_results:  {trans_ref} from {ledger} = {_merge}")
+    print(f"merge_results for {trans_ref} from {ledger} = {_merge}")
 
     if _merge == "both":
         return {"match_result": "matched"}
     else:
         return {"match_result":  "unmatched"}
+
+
+@worker.task(task_type="assign_resolver_worker")
+async def assign_resolver_work(trans_ref,
+                           account,
+                           security_id,
+                           price,
+                           price_currency,
+                           sanctioned_security,
+                           quantity,
+                           trans_type,
+                           amount,
+                           amount_currency,
+                           market,
+                           counter_party,
+                           participant,
+                           settle_date,
+                           actual_settle_date,
+                           source_system,
+                           trade_status,
+                           user_id,
+                           ledger,
+                           _merge
+                           ):
+
+
+    # encode trade values to create X to feed DT predict model
+    # result will come back as OHE
+    X = feature_enc.transform([market, source_system, account])
+    resolver_ohe = dt_model.predict(X)
+
+    # reverse OHE of prediction to get resolver in english
+    resolver = label_enc.inverse_transform(resolver_ohe)
+    a=3
+
+    print(f"assigning resolver for {trans_ref}:  {resolver}")
+
+    return {"ops_resolver": f"{resolver}"}
 
 # Main loop
 loop = asyncio.get_event_loop()
